7bf04aefa47654ecc7e7405aee461923b37fa847
Ismail Alidzhikov <i.alidjikov@gmail.com>
2023-11-16 12:23:53 +0200

    Align the extension according to Gardener's component checklist (#85)
    * extension chart: Define RBAC roles with minimal privileges
    
    * Set the seccomp profile to `RuntimeDefault` for the registry and registry-configuration-cleaner Pods
    
    * Do not run the registry-configuration-cleaner init container in privileged mode
    
    * Add NetworkPolicy labels to the registry cache Pods
    
    Currently, there is no `deny-all` NetworkPolicy created by gardenlet for the kube-system Namespace in the Shoot cluster. See https://github.com/gardener/gardener/blob/v1.82.0/docs/operations/network_policies.md#shoot-cluster.
    That's why it currently works without any NetworkPolicy labels. With this commit, even if a `deny-all` NetworkPolicy is created, the registry cache Pods will continue to work because the corresponding Pods are labeled with the needed NetworkPolicy labels.
    
    * Add PodSecurityPolicy for the registry cache Pods
    
    * Add PodSecurityPolicy for the registry-configuration-cleaner Pods
    
    * Set PriorityClass to the registry-configuration-cleaner Pods
    
    * Add resource requests and limits for the registry-configuration-cleaner Pods

--- pkg/controller/extension/actuator.go
+++ pkg/controller/extension/actuator.go
@@ -107,6 +107,7 @@ func (a *actuator) Reconcile(ctx context.Context, _ logr.Logger, ex *extensionsv
 	registryCaches := registrycaches.New(a.client, namespace, registrycaches.Values{
 		Image:              image.String(),
 		VPAEnabled:         v1beta1helper.ShootWantsVerticalPodAutoscaler(cluster.Shoot),
+		PSPDisabled:        v1beta1helper.IsPSPDisabled(cluster.Shoot),
 		Caches:             registryConfig.Caches,
 		ResourceReferences: cluster.Shoot.Spec.Resources,
 	})
@@ -257,6 +258,7 @@ func cleanRegistryConfiguration(ctx context.Context, cluster *extensionscontroll
 		AlpineImage:       alpineImage.String(),
 		PauseImage:        pauseImage.String(),
 		DeleteSystemdUnit: deleteSystemdUnit,
+		PSPDisabled:       v1beta1helper.IsPSPDisabled(cluster.Shoot),
 		Upstreams:         upstreams,
 	}
 	cleaner := registryconfigurationcleaner.New(client, namespace, values)
