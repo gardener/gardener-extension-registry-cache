a7fda0048243fb73aef72ba60723a284d3167ced
ialidzhikov <i.alidjikov@gmail.com>
2024-01-25 14:38:40 +0200

    hack/update-codegen.sh: Reduce duplication

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -35,7 +35,7 @@ rm -f $GOPATH/bin/*-gen
 PROJECT_ROOT=$(dirname $0)/..
 
 bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
-  deepcopy,defaulter \
+  deepcopy,defaulter,conversion \
   github.com/gardener/gardener-extension-registry-cache/pkg/client \
   github.com/gardener/gardener-extension-registry-cache/pkg/apis \
   github.com/gardener/gardener-extension-registry-cache/pkg/apis \
@@ -43,23 +43,7 @@ bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh 
   --go-header-file "${PROJECT_ROOT}/vendor/github.com/gardener/gardener/hack/LICENSE_BOILERPLATE.txt"
 
 bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
-  conversion \
-  github.com/gardener/gardener-extension-registry-cache/pkg/client \
-  github.com/gardener/gardener-extension-registry-cache/pkg/apis \
-  github.com/gardener/gardener-extension-registry-cache/pkg/apis \
-  "registry:v1alpha1,v1alpha2" \
-  --go-header-file "${PROJECT_ROOT}/vendor/github.com/gardener/gardener/hack/LICENSE_BOILERPLATE.txt"
-
-bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
-  deepcopy,defaulter \
-  github.com/gardener/gardener-extension-registry-cache/pkg/client \
-  github.com/gardener/gardener-extension-registry-cache/pkg/apis \
-  github.com/gardener/gardener-extension-registry-cache/pkg/apis \
-  "config:v1alpha1" \
-  --go-header-file "${PROJECT_ROOT}/vendor/github.com/gardener/gardener/hack/LICENSE_BOILERPLATE.txt"
-
-bash "${PROJECT_ROOT}"/vendor/k8s.io/code-generator/generate-internal-groups.sh \
-  conversion \
+  deepcopy,defaulter,conversion \
   github.com/gardener/gardener-extension-registry-cache/pkg/client \
   github.com/gardener/gardener-extension-registry-cache/pkg/apis \
   github.com/gardener/gardener-extension-registry-cache/pkg/apis \
