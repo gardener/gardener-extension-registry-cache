ec07ef30071e06d15ee969f0c9826eb93fcee160
Dimitar Kostadinov <51451517+dimitar-kostadinov@users.noreply.github.com>
2024-11-25 17:46:43 +0200

    Overwrite registry image ENTRYPOINT to avoid `panic` when `scheduler-state.json` file is corrupted. (#292)
    * Add cleanup-volume init container
    
    * Address PR review feedback
    
    * Overwrite registry image ENTRYPOINT
    
    * Address PR review feedback

--- pkg/component/registrycaches/registry_caches.go
+++ pkg/component/registrycaches/registry_caches.go
@@ -186,6 +186,7 @@ func (r *registryCaches) computeResourcesDataForRegistryCache(ctx context.Contex
 	const (
 		registryCacheVolumeName  = "cache-volume"
 		registryConfigVolumeName = "config-volume"
+		repositoryMountPath      = "/var/lib/registry"
 		debugPort                = 5001
 	)
 
@@ -303,6 +304,32 @@ func (r *registryCaches) computeResourcesDataForRegistryCache(ctx context.Contex
 									corev1.ResourceMemory: resource.MustParse("50Mi"),
 								},
 							},
+							// Mitigation for https://github.com/distribution/distribution/issues/4478:
+							// The registry image entrypoint (https://github.com/distribution/distribution-library-image/blob/be4eca0a5f3af34a026d1e9294d63f3464c06131/Dockerfile#L31)
+							// is extended with a mitigation logic for https://github.com/distribution/distribution/issues/4478.
+							// Keep in sync the registry image entrypoint with the below invocation when updating the registry image version.
+							Command: []string{"/bin/sh", "-c", `REPO_ROOT=` + repositoryMountPath + `
+SCHEDULER_STATE_FILE="${REPO_ROOT}/scheduler-state.json"
+
+if [ -f "${SCHEDULER_STATE_FILE}" ]; then
+    if [ -s "${SCHEDULER_STATE_FILE}" ]; then
+        echo "The scheduler-state.json file exists and it is not empty. Won't clean up anything..."
+    else
+        echo "Detected a corrupted scheduler-state.json file"
+
+        echo "Cleaning up the scheduler-state.json file"
+        rm -f "${SCHEDULER_STATE_FILE}"
+
+        echo "Cleaning up the docker directory"
+        rm -rf "${REPO_ROOT}/docker"
+    fi
+else
+    echo "The scheduler-state.json file is not created yet. Won't clean up anything..."
+fi
+
+echo "Starting..."
+source /entrypoint.sh /etc/distribution/config.yml
+`},
 							Ports: []corev1.ContainerPort{
 								{
 									ContainerPort: constants.RegistryCachePort,
@@ -346,7 +373,7 @@ func (r *registryCaches) computeResourcesDataForRegistryCache(ctx context.Contex
 								{
 									Name:      registryCacheVolumeName,
 									ReadOnly:  false,
-									MountPath: "/var/lib/registry",
+									MountPath: repositoryMountPath,
 								},
 								{
 									Name:      registryConfigVolumeName,
--- pkg/component/registrycaches/registry_caches_test.go
+++ pkg/component/registrycaches/registry_caches_test.go
@@ -226,7 +226,32 @@ spec:
     spec:
       automountServiceAccountToken: false
       containers:
-      - env:
+      - command:
+        - /bin/sh
+        - -c
+        - |
+          REPO_ROOT=/var/lib/registry
+          SCHEDULER_STATE_FILE="${REPO_ROOT}/scheduler-state.json"
+
+          if [ -f "${SCHEDULER_STATE_FILE}" ]; then
+              if [ -s "${SCHEDULER_STATE_FILE}" ]; then
+                  echo "The scheduler-state.json file exists and it is not empty. Won't clean up anything..."
+              else
+                  echo "Detected a corrupted scheduler-state.json file"
+
+                  echo "Cleaning up the scheduler-state.json file"
+                  rm -f "${SCHEDULER_STATE_FILE}"
+
+                  echo "Cleaning up the docker directory"
+                  rm -rf "${REPO_ROOT}/docker"
+              fi
+          else
+              echo "The scheduler-state.json file is not created yet. Won't clean up anything..."
+          fi
+
+          echo "Starting..."
+          source /entrypoint.sh /etc/distribution/config.yml
+        env:
         - name: OTEL_TRACES_EXPORTER
           value: none
         image: ` + image + `
