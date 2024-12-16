f67227207497c42a0435187c37b03d090c6e1aa6
Dimitar Kostadinov <dimitar.kostadinov@sap.com>
2024-12-16 15:18:19 +0200

    Address PR review feedback

--- test/testmachinery/shoot/enable_rotate_ca_disable_test.go
+++ test/testmachinery/shoot/enable_rotate_ca_disable_test.go
@@ -9,7 +9,8 @@ import (
 	"time"
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
-	"github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
+	v1beta1helper "github.com/gardener/gardener/pkg/apis/core/v1beta1/helper"
 	"github.com/gardener/gardener/test/framework"
 	"github.com/gardener/gardener/test/utils/access"
 	. "github.com/onsi/ginkgo/v2"
@@ -21,14 +22,14 @@ import (
 )
 
 const (
-	rotateCertTestTimeout        = 60 * time.Minute
-	rotateCertTestCleanupTimeout = 20 * time.Minute
+	rotateCATestTimeout        = 60 * time.Minute
+	rotateCATestCleanupTimeout = 20 * time.Minute
 )
 
 var _ = Describe("Registry Cache Extension Tests", Label("cache"), func() {
 	f := framework.NewShootFramework(nil)
 
-	f.Disruptive().Beta().CIt("should enable extension, rotate Certificate, disable extension", func(parentCtx context.Context) {
+	f.Disruptive().Beta().CIt("should enable extension, rotate CA, disable extension", func(parentCtx context.Context) {
 		By("Enable the registry-cache extension")
 		ctx, cancel := context.WithTimeout(parentCtx, 10*time.Minute)
 		defer cancel()
@@ -49,30 +50,29 @@ var _ = Describe("Registry Cache Extension Tests", Label("cache"), func() {
 		By("Verify registry-cache works")
 		// Images ghcr.io/jitesoft/alpine:3.18.9, ghcr.io/jitesoft/alpine:3.19.4 and ghcr.io/jitesoft/alpine:3.20.3 are already used by the
 		// "should enable and disable the registry-cache extension" and "should enable extension, hibernate Shoot, reconcile Shoot, wake up Shoot, disable extension"
-		// tests and may present in the Node.
-		// So the current test will use ghcr.io/jitesoft/alpine:3.15.11 ghcr.io/jitesoft/alpine:3.16.9 and ghcr.io/jitesoft/alpine:3.17.9 images.
+		// tests and may be present in the Node.
+		// So the current test will use ghcr.io/jitesoft/alpine:3.15.11, ghcr.io/jitesoft/alpine:3.16.9 and ghcr.io/jitesoft/alpine:3.17.9.
 		common.VerifyRegistryCache(parentCtx, f.Logger, f.ShootClient, common.GithubRegistryJitesoftAlpine31511Image, common.SleepInfinity)
 
-		By("Rotate certificates phase Preparing")
+		By("Start CA rotation")
 		ctx, cancel = context.WithTimeout(parentCtx, 25*time.Minute)
 		defer cancel()
-		Expect(f.AnnotateShoot(ctx, f.Shoot, map[string]string{constants.GardenerOperation: constants.OperationRotateCAStart})).To(Succeed())
+		Expect(f.AnnotateShoot(ctx, f.Shoot, map[string]string{v1beta1constants.GardenerOperation: v1beta1constants.OperationRotateCAStart})).To(Succeed())
 		Expect(f.WaitForShootToBeReconciled(ctx, f.Shoot)).To(Succeed())
-		Expect(f.Shoot.Status.Credentials.Rotation.CertificateAuthorities.Phase).To(Equal(gardencorev1beta1.RotationPrepared))
+		Expect(v1beta1helper.GetShootCARotationPhase(f.Shoot.Status.Credentials)).To(Equal(gardencorev1beta1.RotationPrepared))
 
-		By("Verify registry-cache works when phase is Prepared")
+		By("Verify registry-cache works when CA rotation phase is Prepared")
 		common.VerifyRegistryCache(parentCtx, f.Logger, f.ShootClient, common.GithubRegistryJitesoftAlpine3169Image, common.SleepInfinity)
 
-		By("Rotate certificates phase Completing")
+		By("Complete CA rotation")
 		ctx, cancel = context.WithTimeout(parentCtx, 10*time.Minute)
 		defer cancel()
-		Expect(f.AnnotateShoot(ctx, f.Shoot, map[string]string{constants.GardenerOperation: constants.OperationRotateCAComplete})).To(Succeed())
+		Expect(f.AnnotateShoot(ctx, f.Shoot, map[string]string{v1beta1constants.GardenerOperation: v1beta1constants.OperationRotateCAComplete})).To(Succeed())
 		Expect(f.WaitForShootToBeReconciled(ctx, f.Shoot)).To(Succeed())
-		Expect(f.Shoot.Status.Credentials.Rotation.CertificateAuthorities.Phase).To(Equal(gardencorev1beta1.RotationCompleted))
+		Expect(v1beta1helper.GetShootCARotationPhase(f.Shoot.Status.Credentials)).To(Equal(gardencorev1beta1.RotationCompleted))
 
-		By("Verify registry-cache works after CA rotation")
-		// After rotating the CA, the shoot client must be recreated, otherwise any attempt to use it will fail with::
-		// "tls: failed to verify certificate: x509: certificate signed by unknown authority"
+		By("Verify registry-cache works when CA rotation phase is Completed")
+		// After rotating the CA, the shoot client must be recreated so that it uses the new CA.
 		shootClient, err := access.CreateShootClientFromAdminKubeconfig(ctx, f.GardenClient, f.Shoot)
 		Expect(err).NotTo(HaveOccurred())
 		common.VerifyRegistryCache(parentCtx, f.Logger, shootClient, common.GithubRegistryJitesoftAlpine3179Image, common.SleepInfinity)
@@ -90,9 +90,9 @@ var _ = Describe("Registry Cache Extension Tests", Label("cache"), func() {
 		ctx, cancel = context.WithTimeout(parentCtx, 2*time.Minute)
 		defer cancel()
 		common.VerifyHostsTOMLFilesDeletedForAllNodes(ctx, f.Logger, shootClient, []string{"ghcr.io"})
-	}, rotateCertTestTimeout, framework.WithCAfterTest(func(ctx context.Context) {
-		if f.Shoot.Status.Credentials != nil && f.Shoot.Status.Credentials.Rotation.CertificateAuthorities.Phase != gardencorev1beta1.RotationCompleted {
-			Expect(f.AnnotateShoot(ctx, f.Shoot, map[string]string{constants.GardenerOperation: constants.OperationRotateCAComplete})).To(Succeed())
+	}, rotateCATestTimeout, framework.WithCAfterTest(func(ctx context.Context) {
+		if v1beta1helper.GetShootCARotationPhase(f.Shoot.Status.Credentials) == gardencorev1beta1.RotationPrepared {
+			Expect(f.AnnotateShoot(ctx, f.Shoot, map[string]string{v1beta1constants.GardenerOperation: v1beta1constants.OperationRotateCAComplete})).To(Succeed())
 			Expect(f.WaitForShootToBeReconciled(ctx, f.Shoot)).To(Succeed())
 		}
 		if common.HasRegistryCacheExtension(f.Shoot) {
@@ -103,5 +103,5 @@ var _ = Describe("Registry Cache Extension Tests", Label("cache"), func() {
 				return nil
 			})).To(Succeed())
 		}
-	}, rotateCertTestCleanupTimeout))
+	}, rotateCATestCleanupTimeout))
 })
