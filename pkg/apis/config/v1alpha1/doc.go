10c84f1b57c42ae164f512d6c1ec1793017aaa82
Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>
2025-07-14 17:18:11 +0300

    Unify `gen-crd-api-reference-docs` invocations
    Similar to https://github.com/gardener/gardener/pull/12394

--- pkg/apis/config/v1alpha1/doc.go
+++ pkg/apis/config/v1alpha1/doc.go
@@ -7,7 +7,7 @@
 // +k8s:defaulter-gen=TypeMeta
 // +k8s:openapi-gen=true
 
-//go:generate gen-crd-api-reference-docs -api-dir . -config ../../../../hack/api-reference/config.json -template-dir "$GARDENER_HACK_DIR/api-reference/template" -out-file ../../../../hack/api-reference/config.md
+//go:generate gen-crd-api-reference-docs -api-dir github.com/gardener/gardener-extension-registry-cache/pkg/apis/config/v1alpha1 -config ../../../../hack/api-reference/config.json -template-dir "$GARDENER_HACK_DIR/api-reference/template" -out-file ../../../../hack/api-reference/config.md
 
 // Package v1alpha1 is a version of the API.
 // +groupName=config.registry.extensions.gardener.cloud
--- pkg/apis/registry/v1alpha3/doc.go
+++ pkg/apis/registry/v1alpha3/doc.go
@@ -7,7 +7,7 @@
 // +k8s:defaulter-gen=TypeMeta
 // +k8s:openapi-gen=true
 
-//go:generate gen-crd-api-reference-docs -api-dir . -config ../../../../hack/api-reference/registry.json -template-dir "$GARDENER_HACK_DIR/api-reference/template" -out-file ../../../../hack/api-reference/registry.md
+//go:generate gen-crd-api-reference-docs -api-dir github.com/gardener/gardener-extension-registry-cache/pkg/apis/registry/v1alpha3 -config ../../../../hack/api-reference/registry.json -template-dir "$GARDENER_HACK_DIR/api-reference/template" -out-file ../../../../hack/api-reference/registry.md
 
 // Package v1alpha3 is a version of the API.
 // +groupName=registry.extensions.gardener.cloud
