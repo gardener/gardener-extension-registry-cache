e04f8e62bde6fd285c733cd096340b3198db8530
Martin Stüer <martin.stueer@stackit.cloud>
2026-01-29 17:02:21 +0100

    Add missing securityContext to comply with restricted PSS (#473)
    * Helm chart: add missing securityContext to comply with restricted PSS
    
    * add securityContext settings to registry StatefulSet
    
    * fix tests
    
    * admission helm chart: securityContext
    
    * Address PR review feedback
    
    ---------
    
    Co-authored-by: Ismail Alidzhikov <9372594+ialidzhikov@users.noreply.github.com>

--- pkg/component/registrycaches/registry_caches_test.go
+++ pkg/component/registrycaches/registry_caches_test.go
@@ -322,6 +322,7 @@ proxy:
 								AutomountServiceAccountToken: ptr.To(false),
 								PriorityClassName:            "system-cluster-critical",
 								SecurityContext: &corev1.PodSecurityContext{
+									FSGroup: ptr.To(int64(65532)),
 									SeccompProfile: &corev1.SeccompProfile{
 										Type: corev1.SeccompProfileTypeRuntimeDefault,
 									},
@@ -372,6 +373,14 @@ source /entrypoint.sh /etc/distribution/config.yml
 										Env: env,
 										SecurityContext: &corev1.SecurityContext{
 											AllowPrivilegeEscalation: ptr.To(false),
+											RunAsNonRoot:             ptr.To(true),
+											RunAsUser:                ptr.To(int64(65532)),
+											RunAsGroup:               ptr.To(int64(65532)),
+											Capabilities: &corev1.Capabilities{
+												Drop: []corev1.Capability{
+													"ALL",
+												},
+											},
 										},
 										LivenessProbe: &corev1.Probe{
 											ProbeHandler: corev1.ProbeHandler{
