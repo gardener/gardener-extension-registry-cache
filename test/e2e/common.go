f4e10d25d51745b33dee61d1c27dc37a126d6638
Dimitar Kostadinov <dimitar.kostadinov@sap.com>
2026-09-25 18:48:48 +0300

    Update the Kubernetes version in e2e tests and examples to 1.37.0
    With https://github.com/gardener/gardener/pull/15678 the latest supported Kubernetes version is 1.37.
    
    Signed-off-by: Dimitar Kostadinov <dimitar.kostadinov@sap.com>

--- test/e2e/common.go
+++ test/e2e/common.go
@@ -48,7 +48,7 @@ func DefaultShoot(generateName string) *gardencorev1beta1.Shoot {
 			Region:                 "local",
 			Purpose:                new(gardencorev1beta1.ShootPurposeTesting),
 			Kubernetes: gardencorev1beta1.Kubernetes{
-				Version: "1.36.0",
+				Version: "1.37.0",
 				Kubelet: &gardencorev1beta1.KubeletConfig{
 					SerializeImagePulls: new(false),
 					RegistryPullQPS:     new(int32(10)),
